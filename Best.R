74240fbc63805c583d1369f6514310b36b82f1b3
harshagarwal87 <harshagarwal.kol.1987@gmail.com>
2020-08-01 07:43:00 +0530

    adding Best.R

---
+++ Best.R
@@ -0,0 +1,34 @@
+# Function to find best hospital in a state on basis of given outcome
+# from "outcome-of-care-measures.csv". An error will be generated if 
+# either invalid state or outcome is provided by the user.
+
+
+best <- function(state, outcome) {
+  # Loading data in R and filtering only required columns
+  OutcomeData <- read.csv("outcome-of-care-measures.csv")
+  ReqOutcomeData <- OutcomeData[c(2, 7, 11, 17, 23)]
+  # Creating a vector of column names
+  OutcomeColnames <- c("Hospital", "State", "heart attack", "heart failure", "pneumonia")
+  # Creating a vector of valid outcomes
+  Outcomenames <- OutcomeColnames[3:5]
+  # Checking if state provided by user is valid
+  if (!state %in% ReqOutcomeData$State) 
+    stop("invalid state")
+  # Checking if outcome provided by user is valid
+  else if (!outcome %in% Outcomenames)
+    stop("invalid outcome")
+  else
+    # Splitting dataframe on basis of diferent states
+    StateSplit <- split(ReqOutcomeData, ReqOutcomeData$State)
+    # Subsetting data frame containing rows with user provided state only
+    StateSplitDf <- data.frame(StateSplit[[state]])
+    # Changing column names for newly created data frame
+    colnames(StateSplitDf) <- OutcomeColnames
+    # Coercing required outcome column data to numeric
+    StateSplitDf[, outcome] <- as.numeric(StateSplitDf[, outcome])
+    # Arranging data in ascending order on basis of required outcome
+    StateSplitDf <- StateSplitDf[order(StateSplitDf[outcome]),]
+    # getting value of Hospital from first row
+    BestHospital <- StateSplitDf$Hospital[1]
+    BestHospital
+}
